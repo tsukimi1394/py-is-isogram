b619e041d968ff153342b9174c56168d4d367558
Daria Ganchukova <umi194@gmail.com>
2024-11-03 12:53:10 +0100

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
+import pytest
+from typing import Any
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        None,
+        False,
+        [1],
+        (1),
+        1.1
+    ]
+)
+def test_should_take_only_str(word: Any) -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(word)
+
+
+def test_should_be_case_insensitive() -> None:
+    assert (
+        is_isogram("playgrounds") == is_isogram("PlayGrounds")
+    ), "Function should work case-insensitive"
+
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_should_work_correctly(word: str, result: bool) -> None:
+    assert (
+        is_isogram(word) == result
+    ), "Result is not as expected"
